2404d2797b80f27cccf1e9b644fd8f5c73c6dc88
oesley <oesleyrm@gmail.com>
2023-01-31 16:59:31 -0300

    abcjs implement

--- src/controller/ProgramController.js
+++ src/controller/ProgramController.js
@@ -3,6 +3,12 @@ const path = require('path');
 module.exports = {
 
     async index(req, res) {
+        // var abcString = "X:1\nT:Example\nK:Bb\nBcde|\n";
+        return res.render(path.resolve('src/view/index'),{});
+    },
+
+    async abc(req, res) {
+        // var abcString = "X:1\nT:Example\nK:Bb\nBcde|\n";
         return res.render(path.resolve('src/view/index'),{});
     },
 
